c308986eff34b9ff7741f20ca1308cefcda213c1
Majid Qurashi <106837825+majid-qurashi@users.noreply.github.com>
2025-06-29 20:33:06 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Mushtaq Ah Dar',
   description: 'Blog site',
-  author: 'Majid Qurashi'
+
 };
 
 export default function RootLayout({
